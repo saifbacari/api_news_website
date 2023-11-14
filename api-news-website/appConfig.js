2f8511318f6f29903600c4a29615149dc4595b53
Saifoullah Bacari <saifbacari@gmail.com>
2023-11-14 18:40:15 +0100

    card item changed + filter added on request

--- api-news-website/appConfig.js
+++ api-news-website/appConfig.js
@@ -1,5 +1,7 @@
 const config = {
-    API_KEY: '9790d6f530c648bb9ab9194543ed050f',
+    API_KEY: '6018a23f0acc4f12ab6880e72a5c29d4',
   };
   
   export default config;
+
+  //9790d6f530c48bb9ab9194543ed050f
--- api-news-website/src/components/newsComponents/NewsItem.jsx
+++ api-news-website/src/components/newsComponents/NewsItem.jsx
@@ -9,11 +9,13 @@ const NewsItem = (props) => {
         <li>
           <img className={styles.imgArticle} src={props.urlToImage} />
           <div className={styles.textNewsItem}>
-            <img src={`https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE%2CSIZE%2CURL&url=http://${website}&size=26`} alt={props.source.id}/>
-            <h3 className={styles.nameMedia}>{props.source}</h3>
+            <div className={styles.logoSource}>
+              <img className={styles.logoNews} src={`https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE%2CSIZE%2CURL&url=http://${website}&size=26`} alt={props.source.id}/>
+              <h3 className={styles.nameMedia}>{props.source}</h3>
+            </div>
             <h4 className={styles.titleArticle}>{props.title}</h4>
             <p className={styles.descArticle}>{props.description}</p>
-            <a className={styles.url}>{props.url}</a>
+            <a className={styles.url} href={props.url}>{props.url}</a>
             <h6 className={styles.datePublishing}>{props.publishedAt}</h6>
           </div>
         </li>
